df3e264cc6005ba8f84a6a2d9eddc5aa471dbe11
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-12-30 21:31:47 +0500

    init

--- src/components/MapComponents.tsx
+++ src/components/MapComponents.tsx
@@ -1,4 +1,5 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { useEffect } from "react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 type Props = {
@@ -8,12 +9,18 @@ type Props = {
 
 const MapComponent = ({ Lon, Lat }: Props) => {
   const position: [number, number] = [Lat, Lon];
+
+  const ChangeView = ({ center }: { center: [number, number] }) => {
+    const map = useMap();
+    useEffect(() => {
+      map.setView(center);
+    }, [center, map]);
+    return null;
+  };
+
   return (
-    <MapContainer
-      center={position}
-      zoom={5}
-      style={{ height: "100%", width: "100%" }}
-    >
+    <MapContainer center={position} zoom={5} style={{ height: "100%", width: "100%" }}>
+      <ChangeView center={position} />
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
